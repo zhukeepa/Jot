9fccfa6b0a0a0c0a935059d3597ddd6f643bfced
Alex <zhukeepa@gmail.com>
2015-03-10 15:40:41 -0400

    changed comments

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+  //load up saved value
   var textarea = document.getElementById('content-text');
   chrome.storage.sync.get('value', function(result) {
     textarea.textContent = result.value;
   });
   textarea.focus();
 
-  //there must be some better way
+  //save value on each keypress
   textarea.addEventListener('keyup', function() {
     chrome.storage.sync.set({'value': textarea.value});
   });
