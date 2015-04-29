54e63c2b23f3a63fe0f926785359a48db90243a3
Alex <zhukeepa@gmail.com>
2015-04-29 14:41:04 -0400

    Get messages to send to zhukeepa@gmail.com through jot.mailer@gmail.com

---
+++ background.js
@@ -0,0 +1,14 @@
+// var oauth = ChromeExOAuth.initBackgroundPage({
+//   'request_url': 'https://www.google.com/accounts/OAuthGetRequestToken',
+//   'authorize_url': 'https://www.google.com/accounts/OAuthAuthorizeToken',
+//   'access_url': 'https://www.google.com/accounts/OAuthGetAccessToken',
+//   'consumer_key': 'anonymous',
+//   'consumer_secret': 'anonymous',
+//   'app_name': 'Jot'
+// });
+
+// oauth.authorize(function() {
+//   // ... Ready to fetch private data ...
+// });
+// ''
+// console.log(oauth)
--- popup.js
+++ popup.js
@@ -8,7 +8,31 @@ document.addEventListener('DOMContentLoaded', function() {
   textarea.focus();
 
   //save value on each keypress
-  textarea.addEventListener('keyup', function() {
-    chrome.storage.sync.set({'value': textarea.value});
+  textarea.addEventListener('keyup', function(e) {
+    val = textarea.value; 
+    chrome.storage.sync.set({'value': val});
+    if (e.which == 13) { 
+      if (e.ctrlKey) {
+        e.preventDefault();
+
+        mailToSelf(val, "zhukeepa@gmail.com"); 
+        textarea.value = "";
+        textarea.focus();
+      }
+    }
   });
 });
+
+function mailToSelf(text, email) { 
+  data = { "mail": { "to": email,
+                   "body": text,
+                   "subject": "" }};
+  url = "http://jot-mailer.herokuapp.com/send_mail"; 
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: data,
+    dataType: "text"
+  });
+}
